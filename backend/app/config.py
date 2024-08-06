dcb10a96ee06dc7f89b3ecffbc7a9676636a25ab
Brian <brianzhu4321@gmail.com>
2024-08-06 00:26:44 -0400

    change config file

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sentify.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
