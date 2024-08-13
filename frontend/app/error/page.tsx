a93c1d6fb4c266f66c97109c72fef2f3579e5571
Brian <brianzhu4321@gmail.com>
2024-08-12 23:33:38 -0400

    add error page

---
+++ frontend/app/error/page.tsx
@@ -0,0 +1,7 @@
+export default function Search() {
+    return (
+        <div className="flex-1 flex min-h-screen justify-center items-center">
+            Oops! Something went wrong.
+        </div>
+    );
+}
--- frontend/app/login/actions.ts
+++ frontend/app/login/actions.ts
@@ -15,7 +15,7 @@ export async function login(formData: FormData) {
     const { error } = await supabase.auth.signInWithPassword(data);
 
     if (error) {
-        redirect('/login')
+        redirect('/error')
     }
 
     revalidatePath("/search", "layout");
