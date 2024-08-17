558a5d561f604fe3b8c140e4f143fe31fad7ee8a
Brian <brianzhu4321@gmail.com>
2024-08-17 17:00:38 -0400

    add loading spinner

--- frontend/app/search/layout.tsx
+++ frontend/app/search/layout.tsx
@@ -2,10 +2,12 @@
 
 import { SearchHistoryData } from "@/types";
 import { ReactNode, useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 import { UserSessionProvider } from "@/context/user-session-context";
 import { useUserSession } from "@/context/user-session-context";
 import { Sidebar } from "./components/sidebar";
 import { SheetMenu } from "@/components/admin-panel/sheet-menu";
+import { Spinner } from "@/components/ui/spinner";
 import { fetchSearchHistory } from "./actions/fetch-search-history";
 import { usePathname } from "next/navigation";
 
@@ -51,17 +53,19 @@ function SearchLayoutContent({ children }: { children: ReactNode }) {
 
   return (
     <div className="flex min-h-screen">
-      <div className="m-4">
-        <SheetMenu searchHistory={searchHistory}></SheetMenu>
-      </div>
-      {!loading && searchHistory ? (
-        <Sidebar searchHistory={searchHistory} />
+      {!loading ? (
+        <>
+          <div className="m-4">
+            <SheetMenu searchHistory={searchHistory}></SheetMenu>
+          </div>
+          {searchHistory && <Sidebar searchHistory={searchHistory} />}
+          <div className="flex-1">{children}</div>
+        </>
       ) : (
-        <div className="flex-1 flex justify-center items-center">
-          Loading...
+        <div className="flex items-center justify-center flex-1">
+          <Spinner className={cn("size-24")} strokeWidth={0.6}></Spinner>
         </div>
       )}
-      <div className="flex-1">{children}</div>
     </div>
   );
 }
