a0cd2ca281abadcfd210b716da6df0f3b1c64f95
Brian <brianzhu4321@gmail.com>
2024-08-20 18:30:13 -0400

    implement company api

---
+++ backend/app/api/company.py
@@ -0,0 +1,60 @@
+from flask import jsonify, request, Blueprint
+from models import db, Company
+import requests
+
+company_bp = Blueprint("company", __name__)
+
+
+@company_bp.route("/all/full", methods=["GET"])
+def get_all_companies_full():
+    companies = Company.query.all()
+
+    company_list = [
+        {
+            "id": company.id,
+            "company_name": company.company_name,
+            "ticker": company.ticker,
+            "aliases": company.aliases,
+            "exchange": company.exchange,
+            "currency": company.currency,
+        }
+        for company in companies
+    ]
+
+    return jsonify(company_list), 200
+
+
+@company_bp.route("/all/partial", methods=["GET"])
+def get_all_companies_partial():
+    companies = Company.query.all()
+
+    company_list = [
+        {
+            "id": company.id,
+            "company_name": company.company_name,
+            "ticker": company.ticker,
+            "aliases": company.aliases,
+        }
+        for company in companies
+    ]
+
+    return jsonify(company_list), 200
+
+
+@company_bp.route("/search/<int:company_id>", methods=["GET"])
+def get_company_by_id(company_id: int):
+    company = Company.query.get(company_id)
+
+    if company is None:
+        return jsonify({"message": "Company not found."}), 404
+
+    company_data = {
+        "id": company.id,
+        "company_name": company.company_name,
+        "ticker": company.ticker,
+        "aliases": company.aliases,
+        "exchange": company.exchange,
+        "currency": company.currency,
+    }
+
+    return jsonify(company_data), 200
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,12 @@
 from config import app
 from api.auth import auth_bp
 from api.search import search_bp
+from api.company import company_bp
 from models import db
 
 app.register_blueprint(search_bp, url_prefix="/api/search")
 app.register_blueprint(auth_bp, url_prefix="/api/auth")
+app.register_blueprint(company_bp, url_prefix="/api/company")
 
 if __name__ == "__main__":
     with app.app_context():
