c25018631fd678631541b62a909a672dcd9e8af8
Brian <brianzhu4321@gmail.com>
2024-08-24 00:31:20 -0400

    redirect from register

--- frontend/utils/supabase/middleware.ts
+++ frontend/utils/supabase/middleware.ts
@@ -37,7 +37,11 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (user && request.nextUrl.pathname.startsWith("/login")) {
+  if (
+    user &&
+    (request.nextUrl.pathname.startsWith("/login") ||
+      request.nextUrl.pathname.startsWith("/register"))
+  ) {
     const url = request.nextUrl.clone();
     url.pathname = "/search";
     return NextResponse.redirect(url);
