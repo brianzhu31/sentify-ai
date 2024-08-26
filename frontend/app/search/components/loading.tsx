b0c1478157ed0fff631f6f7c79dda77ba849d9d4
Brian <brianzhu4321@gmail.com>
2024-08-26 18:59:54 -0400

    modify loading

--- frontend/app/search/components/loading.tsx
+++ frontend/app/search/components/loading.tsx
@@ -6,34 +6,26 @@ interface LoadingProps {
   loading: boolean;
 }
 
-const totalSteps = 3;
+const totalSteps = 2;
 
 export function Loading({ loading }: LoadingProps) {
   const [currentStep, setCurrentStep] = useState<number>(1);
 
   useEffect(() => {
     if (loading && currentStep < totalSteps) {
-      const interval = setInterval(() => {
-        setCurrentStep((prevStep) => {
-          if (prevStep < totalSteps) {
-            return prevStep + 1;
-          } else {
-            clearInterval(interval);
-            return prevStep;
-          }
-        });
-      }, 4500);
+      const timeout = setTimeout(() => {
+        setCurrentStep((prevStep) => prevStep + 1);
+      }, 5000);
 
-      return () => clearInterval(interval);
+      return () => clearTimeout(timeout);
     }
-  }, [loading]);
+  }, [loading, currentStep]);
 
   return (
     <div className="flex flex-col gap-2 min-h-screen items-center justify-center flex-1">
       <Spinner className={cn("size-24")} strokeWidth={0.6}></Spinner>
       {currentStep === 1 && <p className="text-2xl ellipsis">Fetching relevant sources</p>}
       {currentStep === 2 && <p className="text-2xl ellipsis">Performing analysis</p>}
-      {currentStep === 3 && <p className="text-2xl ellipsis">Generating output</p>}
       <p>Please hang tight. This process can take up to 30 seconds.</p>
     </div>
   );
