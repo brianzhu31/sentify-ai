87f41c34535c31b42b70434bcef604a36d35fbb9
Brian <brianzhu4321@gmail.com>
2024-08-29 23:59:47 -0400

    modify user and search api + tables

--- backend/app/api/search.py
+++ backend/app/api/search.py
@@ -3,6 +3,7 @@ from services.search_service import get_company_analysis_data
 from models import db, Search, User, Company
 from utils.validation import token_required
 from uuid import UUID
+from datetime import datetime, timedelta
 
 search_bp = Blueprint("search", __name__)
 
@@ -16,7 +17,7 @@ def search_company():
     if user is None:
         return jsonify({"message": "User not found."}), 404
 
-    if user.search_count >= 10:
+    if user.plan == "Basic" and user.daily_search_count >= 10:
         return jsonify({"message": "Daily search limit reached."}), 429
 
     ticker = request.json.get("ticker")
@@ -27,18 +28,24 @@ def search_company():
     if company:
         company_name = company.company_name
         keywords = company.aliases + [company_name, ticker]
-        company_id = company.id
 
         analysis_data = get_company_analysis_data(company_name, keywords, days_ago)
+
+        created_at = datetime.utcnow()
+        data_from = created_at - timedelta(days=days_ago)
 
         new_search = Search(
             company_name=company_name,
             ticker=ticker,
+            overall_summary=analysis_data.get("overall_summary", ""),
             positive_summaries=analysis_data.get("positive", []),
             negative_summaries=analysis_data.get("negative", []),
             top_sources=analysis_data.get("top_sources", []),
             score=analysis_data.get("score", 0),
+            days_range=days_ago,
             created_by=user_id,
+            data_from=data_from,
+            created_at=created_at
         )
 
     else:
@@ -48,7 +55,7 @@ def search_company():
         db.session.add(new_search)
         db.session.commit()
         user.search_ids.append(new_search.id)
-        user.search_count += 1
+        user.daily_search_count += 1
         db.session.commit()
 
     except Exception as e:
@@ -76,27 +83,21 @@ def delete_search(search_id: UUID):
 
     search = Search.query.get(search_id)
     if search is None:
-        return jsonify({"message": "Search not found."}), 404
+        return jsonify({"message": f"Search {search_id} not found."}), 404
 
     if search.created_by != user_id:
         return jsonify({"message": "Unauthorized deletion attempt."}), 403
 
     try:
         Search.query.filter_by(id=search_id).delete()
-        if True:
-            user.search_ids.remove(search_id)
+        user.search_ids.remove(search_id)
         db.session.commit()
 
         return jsonify({"message": f"Search {search_id} successfully deleted."}), 200
 
     except Exception:
         db.session.rollback()
-        return (
-            jsonify(
-                {"message": f"An error occurred with deleting search {search_id}."}
-            ),
-            400,
-        )
+        return jsonify({"message": f"An error occurred with deleting search {search_id}."}), 400
 
 
 @search_bp.route("/get_search/<uuid:search_id>", methods=["GET"])
@@ -104,18 +105,29 @@ def delete_search(search_id: UUID):
 def get_search_by_id(search_id: UUID):
     search = Search.query.get(search_id)
     if search is None:
-        return jsonify({"message": "Search not found."}), 404
+        return jsonify({"message": f"Search {search_id} not found."}), 404
+    
+    company = Company.query.filter_by(ticker=search.ticker).one_or_none()
+    if company is None:
+        return jsonify({"message": "Company not found."}), 404
 
     search_data = {
         "id": search.id,
         "company_name": search.company_name,
         "ticker": search.ticker,
-        "positive_summaries": search.positive_summaries,
-        "negative_summaries": search.negative_summaries,
-        "top_sources": search.top_sources,
-        "score": search.score,
+        "exchange": company.exchange,
+        "currency": company.currency,
+        "days_range": search.days_range,
         "created_by": search.created_by,
+        "data_from": search.data_from,
         "created_at": search.created_at,
+        "analysis_data": {
+            "overall_summary": search.overall_summary,
+            "positive_summaries": search.positive_summaries,
+            "negative_summaries": search.negative_summaries,
+            "top_sources": search.top_sources,
+            "score": search.score
+        }
     }
 
     return jsonify(search_data), 200
--- backend/app/models.py
+++ backend/app/models.py
@@ -11,8 +11,9 @@ db = SQLAlchemy()
 class User(db.Model):
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
     email = Column(String(254), unique=True, nullable=False)
+    plan = Column(String(20), nullable=False, default="Basic")
     search_ids = Column(MutableList.as_mutable(ARRAY(UUID(as_uuid=True))), nullable=True, default=list)
-    search_count = Column(Integer, nullable=False, default=0)
+    daily_search_count = Column(Integer, nullable=False, default=0)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
 
@@ -20,18 +21,21 @@ class Search(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     company_name = Column(String(80), nullable=False)
     ticker = Column(String(6), nullable=False)
+    overall_summary = Column(String(1500), nullable=True)
     positive_summaries = Column(ARRAY(JSON), nullable=False)
     negative_summaries = Column(ARRAY(JSON), nullable=False)
     top_sources = Column(ARRAY(JSON), nullable=False)
     score = Column(Integer, nullable=False)
+    days_range = Column(Integer, nullable=False)
     created_by = Column(String(80), nullable=False)
+    data_from = Column(DateTime, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
 
 class Company(db.Model):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     company_name = Column(String(80), nullable=False)
-    ticker = Column(String(6), nullable=False)
+    ticker = Column(String(6), unique=True, nullable=False)
     aliases = Column(ARRAY(String(80)), nullable=False)
     exchange = Column(String(10), nullable=False)
     currency = Column(String(10), nullable=False)
